8ed776999e570aaa4066cdc51e53f1e17ec2f060
minghao912 <minghaochen912@gmail.com>
2023-12-03 15:11:21 -0800

    chore: remove code scanner on pairing

--- wtr_frontend/src/app/onboard/components/ScanStep.tsx
+++ wtr_frontend/src/app/onboard/components/ScanStep.tsx
@@ -9,34 +9,17 @@ export default function ScanStep(props: { codeRef: React.MutableRefObject<string
 
     return (
         <Flex vertical justify="center" align="center" style={{ height: 'calc(100vh - 190px - 56px - 90px)' }}>
-            <div style={{ height: '250px', width: '250px' }}>
-                {
-                    errorOccurred
-                        ? (
-                            <Space size="middle" direction='vertical' style={{ display: 'flex', flex: 'space-around', alignItems: 'center' }}>
-                                <div style={{ textAlign: 'center' }}>
-                                    <CloseCircleOutlined style={{ fontSize: 100, color: 'red' }} />
-                                </div>
-                                <div style={{ textAlign: 'center', marginTop: '10px' }}>
-                                    <Typography.Text strong>Looks like you don&apos;t have any wtr bottles.</Typography.Text>
-                                    <br />
-                                    <Typography.Text strong>Add one?</Typography.Text>
-                                </div>
-                            </Space>
-                        ): (
-                            <div style= {{ height: '250px', width: '250px', backgroundColor: 'aliceblue' }} />
-                        )
-                }
-            </div>
-            <Divider>or</Divider>
-            <Flex vertical justify="center" align="center" gap={10}>
+            <Flex vertical justify="center" align="center" gap={20}>
+                <Typography.Text strong>
+                    Enter your wtr code
+                </Typography.Text>
                 <Input 
                     showCount
                     maxLength={6}
-                    placeholder="Enter your wtr code"
                     onChange={(e) => props.codeRef.current = e.target.value}
                     onPressEnter={props.nextStep}
                 />
+                <div style={{ height: '30px' }} />
                 <Button onClick={props.nextStep} type="primary">Continue</Button>
             </Flex>
         </Flex>
